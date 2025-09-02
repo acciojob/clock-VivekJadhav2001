5c0bf0f19b670ecb6e546d887700e3fbbbffab27
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-09-02 13:30:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function renderDate(){
+    const date = new Date()
+    document.getElementById("timer").innerHTML = date.toLocaleString()
+}
+setInterval(renderDate,1000)
